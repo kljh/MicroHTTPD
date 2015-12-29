5e37c684e562c4766f5b45eebec4b8df8d70678b
Claude Cochet <code@main2.fr>
2015-12-29 10:20:58 +0100

    tsts

--- run-tests.sh
+++ run-tests.sh
@@ -3,6 +3,8 @@
 curl -i -X OPTIONS http://localhost:8080/ > OPTIONS.txt
 curl -i -X PROPFIND -H "Depth: 0" http://localhost:8080/ > PROPFIND_depth0_root.txt
 curl -i -X PROPFIND -H "Depth: 1" http://localhost:8080/ > PROPFIND_depth1_root.txt
+curl -i -X PROPFIND -H "Depth: 0" http://localhost:8080/mysub/ > PROPFIND_depth0_root_mysub.txt
+curl -i -X PROPFIND -H "Depth: 1" http://localhost:8080/mysub/ > PROPFIND_depth1_root_mysub.txt
 
 git diff HEAD
 
