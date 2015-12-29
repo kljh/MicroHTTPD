25588c63cf0897adb5296be9a94ae5d392288e40
Claude Cochet <code@main2.fr>
2015-12-29 10:52:20 +0100

    tsts

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+debug=~/Documents/Code/vision.git/
+rm $debug/mysub/.DS_Store
+rm $debug/mysub/._hub-hexagon.png 
+
 curl -i -X OPTIONS http://localhost:8080/ > OPTIONS.txt
 curl -i -X PROPFIND -H "Depth: 0" http://localhost:8080/ > PROPFIND_depth0_root.txt
 curl -i -X PROPFIND -H "Depth: 1" http://localhost:8080/ > PROPFIND_depth1_root.txt
